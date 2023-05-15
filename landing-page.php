37fcb74d1bdd37ae087f5c2c3da11ba71cc37fae
andreaconv <andrea2convertini@gmail.com>
2023-05-15 16:12:13 +0200

    end

--- landing-page.php
+++ landing-page.php
@@ -2,6 +2,7 @@
 
 $paragrafo = $_GET['paragrafo'];
 $password = $_GET['password'];
+$paragrafoCensurato = str_replace($password,'***',$paragrafo);
 
 ?>
 
@@ -14,6 +15,7 @@ $password = $_GET['password'];
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
   <title>Badwords</title>
   <style>
     span{
@@ -23,11 +25,17 @@ $password = $_GET['password'];
 </head>
 <body>
 
-  <h1>Paragrafo inserito</h1>
-  <p><?php echo $paragrafo ?></p>
-  <span>Il paragrafo qui sopra è lungo <?php echo strlen($paragrafo) ?> caratteri</span>
+  <div class="container py-5">
+    <h1>Paragrafo inserito</h1>
+    <p><?php echo $paragrafo ?></p>
+    <span>Il paragrafo qui sopra è lungo <?php echo strlen($paragrafo) ?> caratteri</span>
+  
+    <h2>La parola da censurare è <span><?php echo $password ?></span></h2>
+  
+    <h1>Paragrafo censurato</h1>
+    <p><?php echo $paragrafoCensurato ?></p>
 
-  <h2>La parola da censurare è <span><?php echo $password ?></span></h2>
+  </div>
   
 </body>
 </html>
